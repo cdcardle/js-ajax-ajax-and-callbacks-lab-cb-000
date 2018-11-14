263010b07df7eb2ba4becb22d45a2f850650c207
Chris Cardle <cdcardle@gmail.com>
2018-11-14 17:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 $(document).ready(function (){
-});
-function displayError() {
-  $("#errors").html("I'm sorry, there's been an error. Please try again.")
-}
 
-function searchRepositories() {
-  var searchTerms = document.getElementById('searchTerms').value;
-  var url = "https://api.github.com/search/repositories?q=" + searchTerms;
-  $.get(url, function(data){
-    var src = document.getElementById("repo-template").innerHTML;
-    const template = Handlebars.compile(src)
-    const repoList = template(data.items)
-    $("#results").html(repoList)
-  }).fail(error => displayError());
-}
+  function displayError() {
+    $("#errors").html("I'm sorry, there's been an error. Please try again.")
+  }
+
+  function searchRepositories() {
+    const searchTerms = $('#searchTerms').val());
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+      const src = $('#repo-template').innerHTML;
+      const template = Handlebars.compile(src)
+      const repoList = template(data.items)
+      $("#results").html(repoList)
+    }).fail(error => displayError());
+  }
+
+});
