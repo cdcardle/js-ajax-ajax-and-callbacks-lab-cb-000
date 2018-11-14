881458fe1508ca61d30aafc821bc9283359c8225
Chris Cardle <cdcardle@gmail.com>
2018-11-14 16:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 $(document).ready(function (){
 });
+function displayErrors() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  var searchTerms = document.getElementById('searchTerms').value
+  var url = "https://api.github.com/search/repositories?q=" + searchTerms
+  $.get(url, function(data){
+    var src = document.getElementById("repo-template").innerHTML
+    const template = Handlebars.compile(src)
+    const repoList = template(data.items)
+    $("#results").html(repoList)
+  }).fail(error => displayError());
+}
