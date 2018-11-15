e85e652641d66562b2f5f1825c8de0e6c176444c
Chris Cardle <cdcardle@gmail.com>
2018-11-15 17:25:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 $(document).ready(function (){
+});
 
-  function displayError() {
-    $("#errors").html("I'm sorry, there's been an error. Please try again.")
-  }
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
 
-  function searchRepositories() {
-    const searchTerms = $('#searchTerms').val());
-    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
-      const src = $('#repository-template').innerHTML;
-      const template = Handlebars.compile(src)
-      const repoList = template(data.items)
-      $("#results").html(repoList)
-      console.log(template);
-    }).fail(error => displayError());
-  }
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val());
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+    const src = $('#repository-template').innerHTML;
+    const template = Handlebars.compile(src)
+    const repoList = template(data.items)
+    $("#results").html(repoList)
+    console.log(template);
+  }).fail(error => displayError());
+}
 
-  document.addEventListener('DOMContentLoaded', function(event) {
-    Handlebars.registerPartial(
-      'authorPartial',
-      $('author-partial-template').innerHTML
-    );
-  });
-
+document.addEventListener('DOMContentLoaded', function(event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    $('author-partial-template').innerHTML
+  );
 });
 
 // <script id="repo-template" type="text/x-handlebars-template">
