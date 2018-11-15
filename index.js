e9cbac68e39b0a101569ff1e138d40d7a2697a01
Chris Cardle <cdcardle@gmail.com>
2018-11-15 18:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function displayError() {
 function searchRepositories() {
   const searchTerms = $('#searchTerms').val();
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
-    const src = document.getElementById('#repository-template').html;
+    const src = document.getElementById('repository-template').html;
     const template = Handlebars.compile(src);
     const repoList = template(data.items);
     $("#results").html(repoList)
