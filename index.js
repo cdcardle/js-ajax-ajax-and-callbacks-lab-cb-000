de3ce1327831be3136648a3d77b9d335d8bab8c0
Chris Cardle <cdcardle@gmail.com>
2018-11-15 17:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function searchRepositories() {
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  Handlebars.registerPartial('authorPartial', $('author-partial-template').html);
+  Handlebars.registerPartial('authorPartial', $("author-partial-template").html);
 });
 
 // <script id="repo-template" type="text/x-handlebars-template">
