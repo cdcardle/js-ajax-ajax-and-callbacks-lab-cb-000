cb6cec3ddda776d324aa4afd5f963449579eefa6
Chris Cardle <cdcardle@gmail.com>
2018-11-15 17:26:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ function displayError() {
   $("#errors").html("I'm sorry, there's been an error. Please try again.")
 }
 
-// function searchRepositories() {
-//   const searchTerms = $('#searchTerms').val());
-//   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
-//     const src = $('#repository-template').innerHTML;
-//     const template = Handlebars.compile(src)
-//     const repoList = template(data.items)
-//     $("#results").html(repoList)
-//     console.log(template);
-//   }).fail(error => displayError());
-// }
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val());
+  console.log(searchTerms);
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+    const src = $('#repository-template').innerHTML;
+    const template = Handlebars.compile(src)
+    const repoList = template(data.items)
+    $("#results").html(repoList)
+  }).fail(error => displayError());
+}
 
 document.addEventListener('DOMContentLoaded', function(event) {
   Handlebars.registerPartial(
