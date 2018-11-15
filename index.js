9d2ceb53e4c16835654b7fae3c5f2dcbfc198213
Chris Cardle <cdcardle@gmail.com>
2018-11-15 19:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function searchRepositories() {
     let src = document.getElementById('repository-template').innerHTML;
     const template = Handlebars.compile(src);
     const repoList = template(data.items);
-    console.log(template);
+    console.log(repoList);
     $("#results").html(repoList)
   }).fail(error => displayError());
 }
