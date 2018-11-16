479e41b94afee01acdaceb1deb5a87c520454e21
Chris Cardle <cdcardle@gmail.com>
2018-11-16 16:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function searchRepositories() {
 }
 
 function showCommits(el) {
-  console.log(el.dataset);
   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(data) {
     let src = document.getElementById('commits-template').innerHTML;
+    console.log(data);
     const template = Handlebars.compile(src);
     const commitsList = template(data);
     $('details').html(commitsList)
