37584ccab8d6608fb73529458ac4b10b2903e4ec
Chris Cardle <cdcardle@gmail.com>
2018-11-16 15:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ function searchRepositories() {
     let src = document.getElementById('repository-template').innerHTML;
     const template = Handlebars.compile(src);
     const repoList = template(data.items);
-    console.log(data.items);
     $("#results").html(repoList)
   }).fail(error => displayError());
 }
 
-function showCommits() {
-
+function showCommits(el) {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(data) {
+    let src = document.getElementById('commits-template').innerHTML;
+    const template = Handlebars.compile(src);
+    const commitsList = template(data.items);
+    $('details').html(commitsList)
+  }).fail(error => displayError());
 }
